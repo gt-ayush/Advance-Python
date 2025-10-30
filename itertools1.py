f6e23fc17a1fa5d25f7b4eae08032881789b0bbe
gt-ayush <gt-ayush@outlook.com>
2025-10-31 00:52:45 +0530

    itertools -- 7

--- itertools1.py
+++ itertools1.py
@@ -7,3 +7,13 @@ r"""
 PS C:\Users\Ayush Kumar Gupta\Desktop\Advance Python> py .\itertools1.py
 ['S', 'u', 'r', '1', '2', '3', '4', '3', '4']
 """
+
+def prepend(val, iter):
+    return itertools.chain(val,iter)
+print(prepend("a","bcdes"))
+
+r"""
+PS C:\Users\Ayush Kumar Gupta\Desktop\Advance Python> py .\itertools1.py
+['S', 'u', 'r', '1', '2', '3', '4', '3', '4']
+<itertools.chain object at 0x000001325BD75600>
+"""
