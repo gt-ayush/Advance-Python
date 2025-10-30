b136609feba18c9bc4b150f4c49263875b18f695
gt-ayush <gt-ayush@outlook.com>
2025-10-31 00:48:35 +0530

    itertools -- 6

---
+++ itertools1.py
@@ -0,0 +1,9 @@
+import itertools
+
+x= itertools.chain("Sur","123434")
+print(list(x))
+
+r"""
+PS C:\Users\Ayush Kumar Gupta\Desktop\Advance Python> py .\itertools1.py
+['S', 'u', 'r', '1', '2', '3', '4', '3', '4']
+"""
