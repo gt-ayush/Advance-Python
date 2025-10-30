b748b087a34d1336f9566f13982a05c1c1c4e756
gt-ayush <gt-ayush@outlook.com>
2025-10-31 00:03:46 +0530

    more _ iterators -- 1

--- More iterators.py
+++ More iterators.py
@@ -1,5 +1,6 @@
+import itertools
 days=["sun","mon",'tue','wed','thu','fri','sat']
-mon=['jan','feb','mar',"apr",'may','jun','jul']
+mon=['jan','feb','mar',"apr",'may','jun','jul','aug','nov','desc']
 
 # for i in range(len(days)):
 #     print(i+1, days[i])
@@ -12,3 +13,8 @@ mon=['jan','feb','mar',"apr",'may','jun','jul']
 
 for i , d in enumerate(zip(days,mon),start=1):
     print(i,d[0],"=",d[1],"in -------")
+
+r=itertools.zip_longest(days,mon,fillvalue="-")
+
+for i in r:
+    print(i)
