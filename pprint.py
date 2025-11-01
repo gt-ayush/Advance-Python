273da418e058e9be0b4722151f22fb08354dcfe9
gt-ayush <gt-ayush@outlook.com>
2025-11-01 18:30:28 +0530

    pprint --1 with output

--- pprint.py
+++ pprint.py
@@ -9,4 +9,15 @@ print(*val,sep=' -- ')
 for i in range (0,len(val)):
     print(val[i], end=f" [Line: {str(i+1)}]\n")
 
+
+r"""
+PS C:\Users\Ayush Kumar Gupta\Desktop\Advance Python> & "C:/Users/Ayush Kumar Gupta/AppData/Local/Programs/Python/Python314/python.exe" "c:/Users/Ayush Kumar Gupta/Desktop/Advance Python/pprint.py"
+one two three four five
+one -- two -- three -- four -- five
+one [Line: 1]
+two [Line: 2]
+three [Line: 3]
+four [Line: 4]
+five [Line: 5]
+"""
 exit()
