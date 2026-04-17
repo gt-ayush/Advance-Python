b67c266f489d5a2749315c9e6bd7dc0723328735
gt-ayush <gt-ayush@outlook.com>
2026-04-17 15:02:06 +0530

    numpy #7

--- 2d.py
+++ 2d.py
@@ -15,4 +15,9 @@ print(a)
 print("shape:", a.shape)
 
 a = np.array([1, 2, 3, 4, 5, 6])
-a = a.reshape(3, 1)
+a = a.reshape(3, -1) # by specifying one of the dimensions as -1, it will automatically reshape to fit the data.
+print(a)
+print("shape:", a.shape)
+
+a = np.array([1, 2, 3, 4, 5, 6])
+a = a.reshape(4, -1) # Since 6 is not divisible by 4, an error will occur.
